4523abb6b782d78c9215a232d09837a13c91a3f1
Jai Bhardwaj <0987sujals@gmail.com>
2023-01-27 22:16:35 +0530

    Added the routes in App.js for all the pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>  
+        <Route path="/login" element={<Login />}/>  
+        <Route path="/profile/:username" element={<Profile />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
